794dd178357830bb4dd3c35a073249a9b74df006
Waun <waunbroderick@gmail.com>
2018-12-17 14:55:01 +0000

    Added the hotel and flight database connection, and dump to selection page

---
+++ phpFunctions/flights.php
@@ -0,0 +1,36 @@
+<?php
+//Credentials for the Database being used in this specific login.php function
+$servername = "localhost";
+$username = "root";
+$password = "tdteam2018";
+$dbname = "TDVow";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+//SQL call to the database to match usernamd and password exactly
+//$sql = "SELECT buckets.name as name, buckets.description as description, buckets.image as image, buckets.current_price as curr, buckets.total_price as total FROM buckets AS INNER JOIN categories ON categories.id=buckets.category";
+$sql = "SELECT * FROM Flights";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+
+            echo '<p>' .$row["flightname"]. '</p>';
+            echo '<p>' .$row["leaving"]. '</p>';
+            echo '<p>' .$row["destination"]. '</p>';
+            echo '<p>' .$row["price"]. '</p>';
+            echo '<p>' .$row["TDPref"]. '</p>';
+        
+    }
+}else {
+    echo "NO SHIT HOMIE";
+}
+?>
--- phpFunctions/hotels.php
+++ phpFunctions/hotels.php
@@ -16,17 +16,19 @@ if ($conn->connect_error) {
 
 //SQL call to the database to match usernamd and password exactly
 //$sql = "SELECT buckets.name as name, buckets.description as description, buckets.image as image, buckets.current_price as curr, buckets.total_price as total FROM buckets AS INNER JOIN categories ON categories.id=buckets.category";
-$sql = "SELECT Flights.flightname, Flights.leaving, Flight.destination, Flights.price, Flights.id, Flights.TDPref, Flights.image FROM Flights";
+$sql = "SELECT * FROM Hotels";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
 
-            echo '<p>' .$row["flightname"]. '</p>';
-            echo '<p>' .$row["leaving"]. '</p>';
-            echo '<p>' .$row["destination"]. '</p>';
+            echo '<p>' .$row["id"]. '</p>';
+            echo '<p>' .$row["name"]. '</p>';
+            echo '<p>' .$row["description"]. '</p>';
+            echo '<p>' .$row["location"]. '</p>';
             echo '<p>' .$row["price"]. '</p>';
+            echo '<p>' .$row["duration"]. '</p>';
             echo '<p>' .$row["TDPref"]. '</p>';
         
     }
